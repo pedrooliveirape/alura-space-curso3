c3ba7de0ce79b4caba0b97d94d022730c40c86d6
pedrooliveirape <pedro.oliveirape@hotmail.com>
2023-06-15 15:10:34 -0300

    funcao filtro adicionada

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -30,7 +30,7 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, "galeria/buscar.html", {"cards": fotografias})
+    return render(request, "galeria/index.html", {"cards": fotografias})
 
 def nova_imagem(request):
     if not request.user.is_authenticated:
@@ -71,3 +71,7 @@ def deletar_imagem(request, foto_id):
     fotografia.delete()
     messages.success(request, 'Deleção realizada com sucesso!')
     return redirect('index')
+
+def filtro(request, categoria):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria=categoria)
+    return render(request, 'galeria/index.html', {"cards": fotografias})
